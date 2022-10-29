a9a94fceab27ff5af2b1d8426c8c3b1185ce177c
bigjillja <officialmartinbartley@gmail.com>
2022-10-30 02:16:02 +0300

    Final changes to initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+onload=function(){
+    let email = this.document.querySelector('#email')
+    let message = this.document.querySelector('.message')
+
+    addEventListener('submit', (e) => {
+        e.preventDefault()
+        if(email.value == ''){
+            message.innerHTML = 'Please enter a valid email address.'
+            email.value = ''
+        }
+        else{
+            message.innerHTML = `Thank you! Your email address ${email.value} has been added to our mailing list!`
+            email.value = ''
+        }
+    })
+}
